fde0dd5ad1aefa5bb15306eaa5ee1141ccca60ef
Nabila Tsurayya Ahmad <nabilatsurayya323@gmail.com>
2026-09-15 11:55:23 +0700

    feat: tambah interface laporan udara

--- types/cuaca.ts
+++ types/cuaca.ts
@@ -1,13 +1,23 @@
 // types/cuaca.ts
-export interface DataCuaca {
-kota: string;
-suhu: number;
-kelembapan: number;
-catatan?: string;
-}
+
 export type TingkatAQI = "BAIK" | "SEDANG" | "TIDAK_SEHAT" | "BERBAHAYA";
+
+export interface DataCuaca {
+  kota: string;
+  suhu: number;
+  kelembapan: number;
+  catatan?: string;
+}
+
 export interface WeatherCardProps {
-kota: string;
-suhu: number;
-tingkatAQI: TingkatAQI;
+  kota: string;
+  suhu: number;
+  tingkatAQI: TingkatAQI;
+}
+
+export interface LaporanUdara {
+  kota: string;
+  indeksAQI: number;
+  tingkat: TingkatAQI;
+  diperbaruiPada?: string;
 }
